e19d3541764aad74b618b14b14b1e059f4742065
AleliGutierrez <127760432+AleliGutierrez@users.noreply.github.com>
2023-10-10 19:45:11 -0300

    Delete .vscode/ITMASTER directory

--- .vscode/ITMASTER/ITMasterPython/Modulo 1/ejercicio1_ingresoNombre.py
+++
@@ -1,9 +0,0 @@
-"""
-Escribir un programa que permita al usuario ingresar su nombre. 
-El programa debe emitir una salida con un mensaje de bienvenida 
-que incluya el nombre ingresado
-"""
-
-from Recursos.utilidades import saludo, ingresarNombre
-
-saludo(ingresarNombre())      
--- .vscode/ITMASTER/ITMasterPython/Modulo 1/ejercicio36_menu_operaciones.py
+++
@@ -1,8 +0,0 @@
-"""
-Escribir un programa que permita ingresar dos números enteros y la operación a realizar('+', '-', '*', '/'). Debe mostrarse el resultado para la operación ingresada. Considerar que no se puede dividir por cero (en ese caso mostrar el texto 'ERROR').
-
-"""
-
-from Recursos.aritmetica import resultado
-
-print(f"El resultado es: {round(resultado())}")
